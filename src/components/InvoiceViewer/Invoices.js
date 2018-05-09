4714ed9c4d3499e33ea9ed67fb371226b8d4267e
mike <mike@seventythree.cc>
2018-05-09 15:12:16 +0200

    Added filter

--- src/components/InvoiceViewer/Invoices.js
+++ src/components/InvoiceViewer/Invoices.js
@@ -2,24 +2,44 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { getData } from './actions';
 import InvoiceList from './InvoiceList';
+import { ButtonGroup } from 'react-native-elements';
 
 export default class Invoices extends Component {
+  static FILTERS = [ 'Openstaand', 'Alle' ];
   constructor(props) {
     super(props);
-    this.state = { invoices: [] };
+    this.state = {
+      invoices: [],
+      selectedIndex: 0
+    };
+    this.invoices = [];
   }
 
   async componentWillMount() {
-    const invoices = await getData('GETFACT');
-    console.table(invoices);
-    this.setState({ invoices });
+    this.invoices = await getData('GETFACT');
+    console.table( this.invoices );
+    this._filter();
+  }
+
+  _filter = () => {
+    const { selectedIndex } = this.state;
+    const filteredInvoices = selectedIndex === 1 ? this.invoices : this.invoices.filter( invoice => invoice.invoice_paid );
+    this.setState( { invoices: filteredInvoices } );
+  }
+
+  _changeFilter = ( index ) => {
+    this.setState( { selectedIndex: index }, () => this._filter() );
   }
 
   render() {
-    const { invoices } = this.state;
+    const { invoices, selectedIndex } = this.state;
     const { navigate } = this.props.navigation;
     return (
             <View>
+                <ButtonGroup
+                  selectedIndex={ selectedIndex }
+                  onPress={ this._changeFilter }
+                  buttons={ [ Invoices.FILTERS[0], Invoices.FILTERS[1] ] } />
                 <InvoiceList data={ invoices } navigate={ navigate } />
             </View>
     );
